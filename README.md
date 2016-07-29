059aa537af9097120ce45aee62ff79f1faf6b16a
Anthony Gladman <anthony.gladman@gmail.com>
2016-07-29 15:08:24 +0100

    full working version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# omnisearch
-automates pdfgrep searches on PDFs and formats results in log files
+# Omnisearch.py
+This script automates pdfgrep searches on one or more PDFs and formats the results in log files. It also produces a summary of the results so users can quickly tell whether there are any results, and if so how many there are for each search pattern.
+The search patterns are loaded from a text file stored in the same directory.
+## Dependencies
+### getFolio.sh
+Displays a user dialog that asks for the starting folio of the PDF to be searched. This is then used in calculating the folio offset where needed.
+### notify.py
+This script pushes messages to growl notifcations and, if needed, to pushbullet as well. It loads sensitive information such as API tokens from a text file stored in the user's home directory.
--- omnisearch.py
+++ omnisearch.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
 
-"""omnisearch.py automates searches for patterns stored on the file(s)
-    passed from OS X service 'Run Proof searches' TODO: amend the
-    service to run this and not the .sh version.
+"""Omnisearch.py automates searches on one or more PDFs against patterns
+    loaded from the file patterns.txt, which must be stored in the same
+    directory as this script. Other dependencies to be stored in the
+    same directory are getFolio.sh and notify.py.
     """
 
 import datetime
 import logging
-# import notify
+import notify
 import os
 import re
 import sys
 import subprocess
+import time
 
+start_time = time.time()
 search_time = '{:%Y%m%d-%H%M}'.format(datetime.datetime.now())
 
-# logging.disable(logging.CRITICAL)  # comment out this line to enable logging
+logging.disable(logging.CRITICAL)  # comment out this line to enable logging
 logging.basicConfig(
     filename='logs/omnisearch-{0}.txt'.format(search_time),
-    level=logging.DEBUG,
+    level=logging.DEBUG,  # change to .DEBUG for more detailed logging
     format=' %(asctime)s - %(levelname)s - %(message)s')
 logging.info('start of program')
 
@@ -38,6 +41,7 @@ class SearchGroup(object):
         self.parse_output()
         self.results = []  # will hold dependent PatternSearch objects
         self.offset = 0
+        self.match_total = 0
 
     def parse_output(self):
         dirb = os.path.dirname(self.filename)
@@ -65,7 +69,8 @@ class SearchGroup(object):
                 './getFolio.sh', shell=True))
         except Exception as e:
             logging.warning(
-                'error: {0}\nscript will continue without calculating offset'.format(e))
+                'error: {0}\n'
+                'script will continue without calculating offset'.format(e))
             logging.error('continuing with default offset')
         finally:
             pass
@@ -76,13 +81,27 @@ class SearchGroup(object):
 
     def run_searches(self):
         for i, pattern in enumerate(self.patterns):
+            j = i + 1  # to use as num below before it is reassigned as an obj
             i = PatternSearch(pattern, self.filename, self.offset)
             i.call_pdfgrep()
             self.results.append(i)
+            k = len(self.patterns)
+            title = 'omnisearch: {0}'.format(os.path.basename(self.filename))
+            message = '--{0}/{1} complete in {1}s'.format(
+                j, k, str(i.duration))
+            notify.send(title, message)
+        self.count_matches()
+
+    def count_matches(self):
+        sum = 0
+        for obj in self.results:
+            sum += len(obj.match_data)
+        self.match_total = sum
 
 
 class PatternSearch(SearchGroup):
-    """Runs searches for a single pattern, stores and formats results.
+    """Runs searches for a single pattern, stores and formats results,
+        records time taken to perform search.
         """
 
     def __init__(self, pattern, filename, offset):
@@ -92,6 +111,9 @@ class PatternSearch(SearchGroup):
         self.match_data = []  # results of searches with matches
         self.nonmatch_data = []  # list of searches with zero results
         self.output_data = []  # results with matches formatted for output
+        self.start = time.time()
+        logging.debug('start time captured: {0}'.format(self.start))
+        self.duration = 0.00
 
     def call_pdfgrep(self):
         cmd = 'pdfgrep -n "{0}" {1}'.format(
@@ -104,11 +126,11 @@ class PatternSearch(SearchGroup):
                                 shell=True,
                                 universal_newlines=True)
         proc_output = proc.communicate()  # type is tuple
-        logging.debug('proc_output: {0}'.format(proc_output))
+        # logging.debug('proc_output: {0}'.format(proc_output))
         if not proc_output[0] and not proc_output[1]:  # i.e. both parts of tuple are ''
             x = (self.pattern, 'no matches')
             self.nonmatch_data.append(x)
-            logging.debug('tuple appended to nonmatch_data: {0}'.format(x))
+            # logging.debug('tuple appended to nonmatch_data: {0}'.format(x))
         else:
             text = proc_output[0].split('\n') + proc_output[1].split('\n')
             prevline = ''
@@ -120,9 +142,10 @@ class PatternSearch(SearchGroup):
                     else:
                         x = (self.pattern, line)
                         self.match_data.append(x)
-                        logging.debug(
-                            'tuple appended to match_data: {0}'.format(x))
+                        # logging.debug('tuple appended to match_data: {0}'.format(x))
                     prevline = line
+        self.duration = round(time.time() - self.start, 2)
+        logging.debug('duration set to {0}s'.format(self.duration))
 
     def format_output(self):
         # items are tuples, pattern and line
@@ -138,7 +161,7 @@ class PatternSearch(SearchGroup):
                     x = int(pnum) + self.offset
                     pnum = str(x)
                 # add leading zeros
-                while len(pnum) > 4:
+                while len(pnum) < 4:
                     pnum = '0{0}'.format(pnum)
                 # swap order and add pattern
                 output_line = '{0}: {1}: {2}'.format(
@@ -157,9 +180,14 @@ if __name__ == '__main__':
     with open('patterns.txt') as f:
         patterns = f.readlines()
 
+    # capture filepath either as sys arg or from user input
     if len(sys.argv) > 1:
-        filepath = ''.join(sys.argv[1:])
-    else:
+        while True:
+            argv = ''.join(sys.argv[1:])
+            if os.path.exists(si):
+                filepath = argv
+                break
+    if not filepath:
         while True:
             ui = input('Enter filepath: ')
             if os.path.exists(ui):
@@ -168,6 +196,7 @@ if __name__ == '__main__':
             else:
                 print('Check file or folder exists and try again.')
 
+    # populate search queue
     search_queue = []
     cleanpath = os.path.abspath(filepath)
     if os.path.isfile(cleanpath):
@@ -191,9 +220,10 @@ if __name__ == '__main__':
                 pass
         os.chdir(dira)  # back to script home directory
 
+    # perform searches
     SearchGroups = []
     for i, file in enumerate(search_queue):
-        logging.debug('fetched {0} from search_queue at {1}'.format(file, i))
+        logging.debug('fetched {0} from search queue at {1}'.format(file, i))
         i = SearchGroup(file, patterns)
         if len(search_queue) == 1:
             i.fetch_offset()
@@ -201,52 +231,77 @@ if __name__ == '__main__':
         SearchGroups.append(i)
         logging.debug('searches completed for {0}'.format(file))
 
-    print('ran {0} searches on {1} files'.format(
-        len(patterns), len(SearchGroups)))
+    # create summary and output data
+    summary_head = '{0}: ran {1} searches on {2} files'.format(
+        search_time, len(patterns), len(SearchGroups))
+    summary_body = ''
     for obj in SearchGroups:
         nonmatched = []
         body = []
-        print('searches on {0}: '.format(obj.filename))
+        if obj.match_total == 1:
+            match_total_string = '1 result'
+        else:
+            match_total_string = '{0} results'.format(obj.match_total)
+        summary_body += '\n\nsearches on {0}: {1}'.format(
+            os.path.basename(obj.filename), match_total_string)
         for res in obj.results:
             a = len(res.match_data)
             b = len(res.nonmatch_data)
             if b:
                 nonmatched.append(res.pattern)
+                summary_body += '\n\t{0}\t: {1}'.format(
+                    res.pattern, '0'.rjust(4, ' '))
             else:
                 logging.debug('formatting output for {0}, pattern is: {1}.'.format(
                     obj.filename, res.pattern))
-                print('\t{0}\t: {1} results'.format(res.pattern, a))
+                summary_body += '\n\t{0}\t: {1}'.format(
+                    res.pattern, str(a).rjust(4, ' '))
                 res.format_output()
             for line in res.output_data:
                 body.append(line)
-
-        header = '{file}, {time}: {results} results from {searches} searches.\n\n'.format(
+        body.sort()
+        if obj.offset > 0:
+            adjline = '; page nums match print folios; offset is {0}'.format(
+                obj.offset)
+        else:
+            adjline = ''
+        header = '{file}, {time}: {results} results from {searches} searches{adjline}.'.format(
             results=len(body),
             searches=len(patterns),
             file=os.path.basename(obj.filename),
-            time=search_time)
-        footer = '\nPatterns with zero results: \n'
+            time=search_time,
+            adjline=adjline)
+        footer = '\n\nPatterns with zero results: \n'
         for item in nonmatched:
             footer += '\t{0}'.format(item)
 
+        # write output file
         os.chdir(dirb)
         with open(obj.output_file, 'a') as f:
             f.write(header)
+            f.write('\n\n---START---\n')
             for line in body:
                 f.write(line + '\n')
+            f.write('---END---')
             if len(nonmatched) > 0:
                 f.write(footer)
             else:
-                f.write('\nAll searches found matches in the data.')
-            f.write('\n---END---')
+                f.write('\n\nAll searches found matches in the data.')
         os.chdir(dira)
 
+    # produce summary
+    os.chdir(dirb)
+    run_time = round(time.time() - start_time, 2)
+    runtime_string = '\n\nrunning time: {0}s'.format(run_time)
+    summary = summary_head + summary_body + runtime_string
+    summary_file = 'summary-{0}.txt'.format(search_time)
+    with open(summary_file, 'a') as s:
+        s.write(summary)
+    os.chdir(dira)
+    print(summary)
+
+    # finish off and notify
     print('Script complete.')
-    logging.info('end of program')
-
-    # TODO: print other useful information to output file: date, input file
-    # or folder name, start time, total runtime
-
-    # TODO: notify user on progress through script
-    # notify function written and imported but not yet implemented in this script
-    # usage notify.send(title, message, 's', 'p')
+    notify.send('omnisearch: script complete',
+                'running time {0}s'.format(run_time), 's', 'p')
+    logging.info('end of program; running time: {0}s'.format(run_time))
